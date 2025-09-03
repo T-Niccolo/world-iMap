b458ea69dadd299c43230f3422bc471d73fef165
Your NameOrSp <orsp@agri.gov.il>
2025-09-03 22:15:29 +0300

    2

--- iMapMain.py
+++ iMapMain.py
@@ -502,7 +502,7 @@ with col2:
                 if "Imperial" in unit_system:
                     filtered_df[['ET0', 'week_irrigation']] = filtered_df[['ET0', 'week_irrigation']].round(1)
                 else:
-                    filtered_df[['ET0', 'week_irrigation']] = (filtered_df[['ET0', 'week_irrigation']]/5).round()*5
+                    filtered_df[['ET0', 'week_irrigation']] = (filtered_df[['ET0', 'week_irrigation']]/2).round()*2
 
                 st.dataframe(
                     filtered_df[['month', 'ET0', 'week_irrigation', 'alert']]
