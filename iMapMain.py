cac15b5af301affda5ee0fe4d6e38ae88fc3c92f
Your NameOrSp <orsp@agri.gov.il>
2025-09-03 22:10:53 +0300

    -a

--- iMapMain.py
+++ iMapMain.py
@@ -354,7 +354,7 @@ def save_map_as_image_static(lat, lon, zoom=15, size=(600, 450), marker_path='im
 # 🌟 **Streamlit UI**
 st.markdown("<h1 style='text-align: center;'>G-WaB: Geographic Water Budget</h1>", unsafe_allow_html=True)
 st.markdown(
-    "<p style='text-align: center; font-size: 20px'>A <a href=\"https://www.bard-isus.org/\"> <strong>BAaARD</strong></a> research report by: </p>",
+    "<p style='text-align: center; font-size: 20px'>A <a href=\"https://www.bard-isus.org/\"> <strong>BARD</strong></a> research report by: </p>",
     unsafe_allow_html=True)
 st.markdown(
     "<p style='text-align: center;'>For further information contact: <a href=\"mailto:orsp@volcani.agri.gov.il\"> <strong>Or Sperling</strong></a> (ARO-Volcani), <a href=\"mailto:mzwienie@ucdavis.edu\"> <strong>Maciej Zwieniecki</strong></a> (UC Davis), <a href=\"mailto:zellis@ucdavis.edu\"> <strong>Zac Ellis</strong></a> (UC Davis), and <a href=\"mailto:niccolo.tricerri@unito.it\"> <strong>Niccolò Tricerri</strong></a> (UNITO - IUSS Pavia)  </p>",
