23dd258d4a329196f12c6f0fd16ca5dd85c938d5
orsperling <orsp@agri.gov.il>
2025-11-26 12:58:25 +0200

    12

--- iMapMain.py
+++ iMapMain.py
@@ -29,7 +29,7 @@ def initialize_ee():
     # Initialize Earth Engine
     # ee.Initialize(credentials)
 
-# initialize_ee()
+initialize_ee()
 # ee.Initialize()
 # ee.Authenticate()
 # ee.Initialize(project="rsc-gwab-lzp")
