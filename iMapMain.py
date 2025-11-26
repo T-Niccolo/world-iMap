041c1b5c0f26bc9215f8b3aed68669ac2a92a41d
Niccolò Tricerri <152262586+T-Niccolo@users.noreply.github.com>
2025-11-26 13:36:35 +0100

    Uncomment Earth Engine initialization

--- iMapMain.py
+++ iMapMain.py
@@ -27,7 +27,7 @@ def initialize_ee():
         scopes=["https://www.googleapis.com/auth/earthengine"]
     )
     # Initialize Earth Engine
-    # ee.Initialize(credentials)
+    ee.Initialize(credentials)
 
 initialize_ee()
 # ee.Initialize()
